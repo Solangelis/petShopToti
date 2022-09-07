8888c2207780878ba3c5bcc62f31e508f4ba06b1
Tabitha Philistin Destin <104877297+Shabinejosias@users.noreply.github.com>
2022-09-07 12:48:39 -0300

    inicial o get

--- servidor/src/Routes/index.routes.js
+++ servidor/src/Routes/index.routes.js
@@ -3,6 +3,12 @@ const Router = require ('express')
 const router = Router()
 router.post('/api/produtos', (req, resp)=>{
     resp.send('produto criado com sucesso')
+
 }) 
+router.get('/api/produtos', (req, res) =>{
+    res.send('todos os produtos')
+})
+
+
 
 module.exports=router

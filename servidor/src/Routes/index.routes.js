f8aa945b4ed2213362c940ed10847ba7596d67b9
serranoankys <serranoankys@gmail.com>
2022-09-07 13:52:27 -0300

    Editar funcionalidade de um produto

--- servidor/src/Routes/index.routes.js
+++ servidor/src/Routes/index.routes.js
@@ -6,21 +6,25 @@ const router = Router()
 router.post('/api/produtos', (req, resp) => {
     const produto = produtoModel(req.body)
     produto.save()
-    .then((dado)=>resp.status(200).json(dado))
-    .catch((err)=>resp.status(400).json({message: err}).end())
+        .then((dado) => resp.status(200).json(dado))
+        .catch((err) => resp.status(400).json({ message: err }).end())
 })
 
 //Listar todos os Produtos
-router.get('/api/produtos', (req, res) => {
+router.get('/api/produtos', (req, resp) => {
     produto.find()
-    .then((dado)=>resp.status(200).json(dado))
-    .catch((err)=>resp.status(400).json({message: err}).end())
+        .then((dado) => resp.status(200).json(dado))
+        .catch((err) => resp.status(400).json({ message: err }).end())
 })
 
 
 //Listar um só Produto
 router.get('/api/produtos/:id', (req, res) => {
-    res.send('so um produto')
+    const { id } = req.params
+
+    produto.findById(id)
+        .then((dado) => resp.status(200).json(dado))
+        .catch((err) => resp.status(400).json({ message: err }).end())
 })
 
 //Deletar Produto

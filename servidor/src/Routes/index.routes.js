938419a5c907a91ac955c5c833b4f3426f2f4029
solangelis <solangelis110493@hotmail.com>
2022-09-07 13:39:30 -0300

    corrigindo metodo listar todos

--- servidor/src/Routes/index.routes.js
+++ servidor/src/Routes/index.routes.js
@@ -12,8 +12,11 @@ router.post('/api/produtos', (req, resp) => {
 
 //Listar todos os Produtos
 router.get('/api/produtos', (req, res) => {
-    res.send('todos os produtos')
+    produto.find()
+    .then((dado)=>resp.status(200).json(dado))
+    .catch((err)=>resp.status(400).json({message: err}).end())
 })
+
 
 //Listar um só Produto
 router.get('/api/produtos/:id', (req, res) => {

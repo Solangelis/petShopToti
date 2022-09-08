fa87b376b382c4cdd98e89c6ea7a462b8f3a1d65
Solangelis <solangelis110493@gmail.com>
2022-09-08 01:16:09 -0300

    adicionando arquivos para usar no nosso footer

--- cliente/src/App.jsx
+++ cliente/src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-
+import Footer from './Components/Footer/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
       <h1>Seja Benvido a Nossa App</h1>
       <h2>A nossa página se encontra em Construcao</h2>
+      <Footer />
     </div>
   )
 }

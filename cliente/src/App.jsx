6839f4aa299763d01a76d9187f2335222b6b2680
serranoankys <serranoankys@gmail.com>
2022-09-08 16:22:34 -0300

    commit navbar

--- cliente/src/App.jsx
+++ cliente/src/App.jsx
@@ -1,15 +1,19 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footer'
+
 
 function App() {
 
   return (
     <div className="App">
+      <Navbar />
       <h1>Seja Benvido a Nossa App</h1>
       <h2>A nossa página se encontra em Construcao</h2>
       <Footer />
+
     </div>
   )
 }

24727d357c202fd3aa712d4564564aa8b22dc7f7
Humberto Colmenares <colmenareshr@gmail.com>
2022-09-11 15:56:29 -0300

    estilos al footer

--- cliente/src/pages/Home.jsx
+++ cliente/src/pages/Home.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div><h1>Seja Benvindos a Nossa Home page</h1></div>
+    <>      
+      <h1>Seja Benvindos a Nossa Home page</h1>
+
+      
+      </>
   )
 }

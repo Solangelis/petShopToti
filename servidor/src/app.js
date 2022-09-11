6b9dfbed1d09bd8ea1745a9aac2d0381466f6584
Humberto Colmenares <colmenareshr@gmail.com>
2022-09-11 11:50:52 -0300

    agregando cors

--- servidor/src/app.js
+++ servidor/src/app.js
@@ -1,17 +1,19 @@
 const express = require('express')
+const cors = require('cors')
 const rotas = require('./Routes/index.routes') 
 const morgan = require('morgan')
 
 
 const app = express()
-
+app.use(cors())
 
 //middelwares
 app.use(morgan('dev'))
 app.use(express.json())
-
-app.use(rotas)
 app.use(express.urlencoded({extended: false}))
+
+//Rotas
+app.use(rotas)
 
 
 module.exports = app

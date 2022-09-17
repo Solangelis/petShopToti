0d985b428b0dad4446639ac85ce63c26c4cb8767
Marisa Basilio <edna189@hotmail.com>
2022-09-17 15:53:16 -0300

    alterando o titulo about

--- cliente/src/components/About/about.jsx
+++ cliente/src/components/About/about.jsx
@@ -9,12 +9,12 @@ function About() {
     return (
         <main>
             <div className='home-about'>
+                <h1 id='title'>Sobre nós - Fofinhos Pet Shop</h1>
                 <div className='container-resumo-fotos'>
                     <div className='div-imagem'>
                     </div>
                     <img className='imgem-home' src={about}  alt='about'/>
                     <div className='div-resumo'>
-                        <h1 id='title'>Sobre nós - Fofinhos Pet Shop</h1>
                         <p>Fundada em 2022, a Fofinhos Pet Shop se dedica pela qualidade e variedades de produtos e serviços voltados aos animais. Entre eles oferecemos tratamentos completos para cães e gatos
                     Em nosso espaço o cliente encontra tudo o que precisa para seu animal, como brinquedos, acessórios e alimentação. </p>
                     O projeto foi desenvolvido por 5 estudantes de diferentes nacionalidades, tais como: Angola, Haiti, Venezuela.

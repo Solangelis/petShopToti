beeb192f06981791065812fbde173c51d231aefb
Tabitha Philistin Destin <104877297+Shabinejosias@users.noreply.github.com>
2022-09-16 23:32:35 -0300

    criando CardHome

--- cliente/src/components/CardHome/CardHome.jsx
+++ cliente/src/components/CardHome/CardHome.jsx
@@ -5,7 +5,7 @@ import imagepet from './imagCard/imagepet.jpg'
 export function CardHome() {
     return(
        <div>
-        <h1>Estamos sempre aqui pela boa saúde de todos os seus Pets</h1>
+        <h1>O que os clientes falam sobre nossa empressa </h1>
 <p>Os clientes falam que eles sentem com alegria quando os pets são áqui</p>
 <img src={imagepet} alt='imagepet' width ={'40px'} />
        </div> 

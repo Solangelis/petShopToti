d534c30b778988031c5af160d3a5d0ff7da9d0d0
Marisa Basilio <edna189@hotmail.com>
2022-09-18 13:33:02 -0300

    adicionando imagem ao cardHome

--- cliente/src/components/About/about.jsx
+++ cliente/src/components/About/about.jsx
@@ -15,11 +15,12 @@ function About() {
                     </div>
                     <img className='imgem-home' src={about}  alt='about'/>
                     <div className='div-resumo'>
-                        <p>Fundada em 2022, a Fofinhos Pet Shop se dedica pela qualidade e variedades de produtos e serviços voltados aos animais. Entre eles oferecemos tratamentos completos para cães e gatos
-                    Em nosso espaço o cliente encontra tudo o que precisa para seu animal, como brinquedos, acessórios e alimentação. </p>
-                    O projeto foi desenvolvido por 5 estudantes de diferentes nacionalidades, tais como: Angola, Haiti, Venezuela.
-                    Projeto final  do curso de desvenvolvedor Full Stack  na Toti. Onde cada grupo elaborou seu próprio projeto, incluindo design e conteúdo. No projeto apresentado aqui, foi utilizado conhecimentos em linguagem de programação.
-                    <p/>
+                        <p>Fundada em 2022, a Fofinhos Pet Shop se dedica pela qualidade e variedades de produtos e serviços voltados aos animais. Entre eles oferecemos tratamentos completos para cães e gatos.
+                        Em nosso espaço o cliente encontra tudo o que precisa para seu animal, como brinquedos, acessórios e alimentação.
+                        </p>O projeto foi desenvolvido por 5 estudantes de diferentes nacionalidades, tais como: Angola, Haiti e Venezuela.
+                        Projeto final  do curso de desvenvolvedor Full Stack na Toti. Onde cada grupo elaborou seu próprio projeto, incluindo design e conteúdo. No projeto apresentado aqui, foi utilizado conhecimentos em linguagem de programação.
+                        <p/>
+                        <br />
                     <button className='btn-about'>Saiba Mais</button>
                     </div>
                 </div>
--- cliente/src/components/CardHome/CardHome.jsx
+++ cliente/src/components/CardHome/CardHome.jsx
@@ -1,15 +1,20 @@
 import React from "react";
-import imagepet from  '../images/imagepet.png'
+import imagepet from  '../images/imagepet.png';
+import cate from '../images/cate.png';
 import './CardHome'
 
 
 export function CardHome() {
     return(
-       <div>
+    <div>
         <h2>A melhor ração, para sua melhor companhia! ...</h2>
-<p>Compartilhe fotos e vídeos de bichinhos antes e depois de passarem pelo banho e tosa do seu estabelecimento</p>
-<img src={imagepet} alt='imagepet' />
-       </div> 
+        <p>Compartilhe fotos e vídeos de bichinhos antes e depois de passarem pelo banho e tosa do seu estabelecimento</p>
+        <div className="pet-cachorro">
+            <img src={imagepet} alt='imagepet' />
+            <img src={cate} alt='cate'/>
+        </div>
+    </div>
+    
     )
 }
 export default CardHome;

94c7baa9e6d148a8737cc3ed12e8e63b35cb7c33
Humberto Colmenares <colmenareshr@gmail.com>
2022-09-21 16:35:23 -0300

    agregando resposividad

--- cliente/src/components/CategoryCards/CategoryCards.jsx
+++ cliente/src/components/CategoryCards/CategoryCards.jsx
@@ -2,13 +2,13 @@ import '../CategoryCards/CategoryCards.css';
 
 export default function CategoryCards({image, categoria}) {
   return (
-    <>
-      <div className='category_wrapper'>
+    
+      <>
         <div className='category_cards'>
           <img src={`../../public/img/category-pet${image}.jpg`} alt='dog' />
           <h3>{categoria}</h3>
         </div>
-      </div>
-    </>
+      </>
+    
   );
 }
